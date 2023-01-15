560eaec761467c80d11781ecfa9e184f9e0c0e99
Eugene <roundcubez@gmail.com>
2023-01-15 19:38:42 +0200

    Round small values correctly

--- stocks@infinicode.de/helpers/data.js
+++ stocks@infinicode.de/helpers/data.js
@@ -71,13 +71,13 @@ var getComplementaryColor = (hex, bw = true) => {
     throw new Error('Invalid HEX color.')
   }
   var r = parseInt(hex.slice(0, 2), 16),
-      g = parseInt(hex.slice(2, 4), 16),
-      b = parseInt(hex.slice(4, 6), 16)
+    g = parseInt(hex.slice(2, 4), 16),
+    b = parseInt(hex.slice(4, 6), 16)
   if (bw) {
     // http://stackoverflow.com/a/3943023/112731
     return (r * 0.299 + g * 0.587 + b * 0.114) > 186
-        ? '#000000'
-        : '#FFFFFF'
+      ? '#000000'
+      : '#FFFFFF'
   }
   // invert color components
   r = (255 - r).toString(16)
@@ -95,4 +95,16 @@ var moveDecimal = (value, decimalPlaces) => {
   return value / Math.pow(10, decimalPlaces)
 }
 
-var roundOrDefault = (number, defaultValue = '--') => isNullOrUndefined(number) ? defaultValue : (Math.round((number + Number.EPSILON) * 100) / 100).toFixed(2)
+var roundOrDefault = (number, defaultValue = '--') => {
+  const value = Number(number);
+
+  if (isNaN(value)) {
+    return defaultValue;
+  }
+
+  if (value < 1) {
+    return value.toPrecision(2);
+  }
+
+  return value.toFixed(2);
+}
